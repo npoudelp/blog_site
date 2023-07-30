9831b4ef3809399abac08b5884d780f278184a01
npoudelp <npoudelp@gmail.com>
2023-07-30 13:27:59 +0545

    basic get, get_by_id, put request handeler made

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from api import views
+from api import apis
 
 
 urlpatterns = [
-    path('', views.get_all, name="get_all"),
+    path('', apis.get_all, name='get_all'),
+    path('get_by_id/<int:id>', apis.get_by_id, name='get_by_id'),
+    path('post_blog/', apis.post_blog, name='post_blog'),
+    path('edit_blog/<int:id>', apis.edit_blog, name='edit_blog'),
 ]
